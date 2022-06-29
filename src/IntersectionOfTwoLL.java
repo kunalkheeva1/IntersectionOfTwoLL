677fdcc4c4fad38fd65b138f008c268e70f95312
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 03:09:30 -0700

    created a node class

--- src/IntersectionOfTwoLL.java
+++ src/IntersectionOfTwoLL.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+
 public class IntersectionOfTwoLL {
-    
+
+     static class Node{
+         int data;
+         Node next;
+         Node(int data){
+             this.data = data;
+             this.next = null;
+         }
+
+         public static Node intersectionOfTwoLL(Node headA, Node headB){
+             HashMap<Node, Boolean> map = new HashMap<>();
+
+         }
+     }
+
     public static void main(String[] args) {
 
     }
