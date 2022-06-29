dbab6a11add4a04e9a95b2161cd04d22e78265cb
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 03:14:30 -0700

    added comments for better grip on it

--- src/IntersectionOfTwoLL.java
+++ src/IntersectionOfTwoLL.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 public class IntersectionOfTwoLL {
-
+    //get the node
      static class Node{
          int data;
          Node next;
@@ -9,15 +9,15 @@ public class IntersectionOfTwoLL {
              this.data = data;
              this.next = null;
          }
-
+        //list one and two
          public static Node intersectionOfTwoLL(Node headA, Node headB){
              HashMap<Node, Boolean> map = new HashMap<>();
-
+            //put list A in map
              while(headA != null){
                  map.put(headA, true);
                  headA = headA.next;
              }
-
+            //now if at any point map contains the node of B, then return it otherwise go ahead
              while (headB != null){
                  if(map.containsKey(headB)){
                     return headB;
